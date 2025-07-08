05c8b9d6538f3dddd8848485cd62a581b9271d6d
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-07-08 08:08:32 +0000

    [add] 3-12 バリテーション、コントローラーの中身、トップ画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,43 @@
-class BooksController < ApplicationController
+class ListsController < ApplicationController
   def new
+      @books = Books.new
+  end
+
+  def create
+    @books = books.new(books_params)
+    if @books.save
+      flash[:notice] = "success"
+      redirect_to books_path(@books.id)
+    else
+      flash.now[:alert] = "error" 
+      render :new
+    end
   end
 
   def index
-  end
-
-  def show
+    @books = Books.all
   end
 
   def edit
+    @books = books.find(params[:id])
+  end
+
+  def update
+    books = Books.find(params[:id])
+    books.update(books_params)
+    redirect_to books_path(books.id)  
+  end
+
+  def destroy
+    books = books.find(params[:id])  
+    books.destroy 
+    redirect_to '/books/index'  
+  end
+
+  private
+
+  # ストロングパラメータ
+  def books_params
+    params.require(:books).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'books/new'
+  post 'books' => 'books#create'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  get '/bookers' => 'homes#top'
+  get '/bookers' => 'homes#bookers'
 end
